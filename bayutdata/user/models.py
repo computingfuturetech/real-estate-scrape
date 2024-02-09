6286d9908c872c65c79decf4fd75807ad4c9d4dd
anasircft <anasircft@gmail.com>
2024-02-09 15:58:41 +0500

    alter field phone number

--- bayutdata/user/models.py
+++ bayutdata/user/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email=models.EmailField(unique=True)
-    phone=models.CharField(max_length=11)
+    phone=models.CharField(max_length=11,null=True, blank=True,)
     date_of_birth=models.DateField(null=True, blank=True,)
     def __str__(self):
         return self.username
