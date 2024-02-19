410c56121aa89b66478fd0ebe7efd79cbf5ab6a9
anasircft <anasircft@gmail.com>
2024-02-19 18:25:55 +0500

    add two factor authentication

--- bayutdata/dashboard/models.py
+++ bayutdata/dashboard/models.py
@@ -1,8 +1,5 @@
 from django.db import models
 
-
-
-
 class ApartmentDetail(models.Model):
     apartment_id = models.IntegerField(unique=True)
     price = models.IntegerField()
@@ -27,3 +24,23 @@ class BuildingInformation(models.Model):
 
     def __str__(self):
         return str(self.building_id)
+
+class ProjectInformation(models.Model):
+    project_id= models.IntegerField(unique=True)
+    project_name = models.CharField(max_length=500)
+    last_inspected=models.CharField(max_length=500)
+    completion=models.CharField(max_length=50)
+    handover=models.CharField(max_length=500)
+
+
+class ValidatedInformation(models.Model):
+    validated_id= models.IntegerField(unique=True)
+    developer = models.CharField(max_length=500)
+    ownership=models.CharField(max_length=500)
+    built_up_area=models.CharField(max_length=50)
+    usage=models.CharField(max_length=500)
+    balcony_size=models.CharField(max_length=500)
+    total_building_area=models.CharField(max_length=500)
+    parking_availability=models.CharField(max_length=500)
+
+
